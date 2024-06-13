6bfa992e7240b9b06ca87fac99d0434a2d9b54d4
CHETAN SHARMA <123791402+Chetan1704@users.noreply.github.com>
2024-06-13 20:58:28 +0530

    Add files via upload

---
+++ Todo_List-main/script.js
@@ -0,0 +1,54 @@
+
+
+// Function to add a new task
+function addTask() {
+    const taskInput = document.getElementById('task-input');
+    const taskText = taskInput.value.trim();
+
+    if (taskText !== "") {
+        const taskUl = document.getElementById('task-ul');
+
+        const taskLi = document.createElement('li');
+        taskLi.textContent = taskText;
+
+        const taskButtons = document.createElement('div');
+        taskButtons.className = 'task-buttons';
+
+        const completeButton = document.createElement('button');
+        completeButton.className = 'complete';
+        completeButton.textContent = 'Complete';
+        completeButton.onclick = () => {
+            taskLi.classList.toggle('complete');
+        };
+
+        const deleteButton = document.createElement('button');
+        deleteButton.className = 'delete';
+        deleteButton.textContent = '+';
+
+        deleteButton.onclick = () => {
+            taskUl.removeChild(taskLi);
+        };
+
+        taskButtons.appendChild(completeButton);
+        taskButtons.appendChild(deleteButton);
+        taskLi.appendChild(taskButtons);
+
+        taskUl.appendChild(taskLi);
+
+        taskInput.value = '';
+    }
+}
+function vari(){
+    
+   
+      VanillaTilt.init(document.querySelector("#todo"), {
+          max: 4,
+          speed: 800,
+      scale: 1.03,
+      glare: true,
+      "max-glare": 0.5,
+      });
+   
+}
+vari();
+
